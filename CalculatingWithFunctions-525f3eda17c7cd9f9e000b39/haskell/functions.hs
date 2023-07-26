d74d9e4da3988ac028358449739680f0e66d2cdc
ni-sgn <saganeli.nika@gmail.com>
2023-07-26 15:55:01 +0400

    best kata yet

--- CalculatingWithFunctions-525f3eda17c7cd9f9e000b39/haskell/functions.hs
+++ CalculatingWithFunctions-525f3eda17c7cd9f9e000b39/haskell/functions.hs
@@ -1,22 +1,22 @@
 module CalculatingWithFunctions where
 
-plus,minus,times,dividedBy :: _ 
-plus = undefined 
-minus = undefined 
-times = undefined 
-dividedBy = undefined
+plus,minus,times,dividedBy :: ((Int -> Int) -> Int) -> (Int -> Int)
+plus  f     = (+) (f id) 
+minus f     = flip  (-) (f id) 
+times f     = (*) (f id) 
+dividedBy f = flip (div) (f id)
 
-zero,one,two,three,four,five,six,seven,eight,nine :: _ 
-zero = undefined  
-one = undefined
-two = undefined
-three = undefined
-four = undefined
-five = undefined
-six = undefined
-seven = undefined
-eight = undefined
-nine = undefined
+zero,one,two,three,four,five,six,seven,eight,nine :: (Int -> Int) -> Int 
+zero  f = f 0  
+one   f = f 1
+two   f = f 2
+three f = f 3
+four  f = f 4
+five  f = f 5
+six   f = f 6
+seven f = f 7
+eight f = f 8
+nine  f = f 9
 
 main :: IO ()
-main = undefined
+main = print $ two $ times $ four 
